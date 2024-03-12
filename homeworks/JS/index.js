2864e97afcfd353590b21114df5ce1b9b3c5c7ef
Serhiy Hourwitz <shmone800@gmail.com>
2024-03-12 20:52:21 +0200

    Add default to prompt

--- homeworks/JS/index.js
+++ homeworks/JS/index.js
@@ -1,16 +1,16 @@
 const greeting = alert('Hi, Arthur!');
 
-const isAdult = confirm("Is your age > 18 ?");
+const isAdult = confirm('Is your age > 18 ?');
 
 // Name
 
-const userName = prompt("What is your name ?");
+const userName = prompt('What is your name ?');
 
 console.log(`Hello, ${userName} !`);
 
 // Age
 
-const yearOfBirth = prompt("What is Your year of birth?");
+const yearOfBirth = prompt('What is Your year of birth?', 1966);
 const currentYear = 2024;
 const age = currentYear - yearOfBirth;
 
@@ -18,26 +18,26 @@ console.log(`You are ${age}.`);
 
 // Perimeter
 
-const length = prompt("What is a length of square side ?");
+const length = prompt('What is a length of square side ?');
 const perimeter = length * 4;
 
 console.log(`Perimeter of square is ${perimeter} units.`);
 
 // Square
 
-const radius = prompt("What is a radius of a round ?");
+const radius = prompt('What is a radius of a round ?');
 const squareOfRound = Math.PI * radius ** 2;
 
 console.log(`Square of a round is ${squareOfRound} square units.`);
 
 // Speed
 
-const cityStart = prompt("What is your city ?");
-const cityFinish = prompt("What is a city of destination ?");
+const cityStart = prompt('What is your city ?');
+const cityFinish = prompt('What is a city of destination ?');
 const distance = prompt(
   `What is a distance between ${cityStart} & ${cityFinish} in km?`
 );
-const time = prompt("How many hours do You plan to drive?");
+const time = prompt('How many hours do You plan to drive?');
 const speed = distance / time;
 
 console.log(
@@ -46,7 +46,7 @@ console.log(
 
 // Currency exchange
 
-const usdAmount = prompt("How match dollars do You want to exchange ?");
+const usdAmount = prompt('How match dollars do You want to exchange ?');
 const euroUsdRate = 0.902;
 const euroAmount = usdAmount * euroUsdRate;
 
