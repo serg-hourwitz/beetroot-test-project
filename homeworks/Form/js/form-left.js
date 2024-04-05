953d5b4f9f0da6aa8606e4ffd599b175f1fe7bbd
Serhiy Hourwitz <shmone800@gmail.com>
2024-04-05 19:11:47 +0300

    added fetch

--- homeworks/Form/js/form-left.js
+++ homeworks/Form/js/form-left.js
@@ -1,15 +1,14 @@
-import setError from "./error.js";
+import setError from './error.js';
 
 const registrationFormLeft = () => {
-  const DOMFormLeft = document.getElementById("js-form-left");
+  const DOMFormLeft = document.getElementById('js-form-left');
   const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const CITY_REGEX = /^[a-zA-Z\s]*$/;
   const leftFormDataArr = [];
 
-
   if (!DOMFormLeft) return;
 
-  DOMFormLeft.addEventListener("submit", (e) => {
+  DOMFormLeft.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const formLeft = new FormData(e.target);
@@ -17,98 +16,146 @@ const registrationFormLeft = () => {
     const leftFormDataObj = {};
 
     // first name
-    const fname = formLeft.get("fname");
+    const fname = formLeft.get('fname');
 
     if (!fname) {
-      return setError("js-left-form-fname-error", "First name is required");
+      return setError('js-left-form-fname-error', 'First name is required');
     }
 
     if (fname.length < 3) {
       return setError(
-        "js-left-form-fname-error",
-        "First name must be at least 3 characters"
+        'js-left-form-fname-error',
+        'First name must be at least 3 characters'
       );
     }
-    setError("js-left-form-fname-error", "");
+    setError('js-left-form-fname-error', '');
 
     leftFormDataObj.fname = fname;
 
     // last name
-    const lname = formLeft.get("lname");
+    const lname = formLeft.get('lname');
 
     if (!lname) {
-      return setError("js-left-form-lname-error", "Last name is required");
+      return setError('js-left-form-lname-error', 'Last name is required');
     }
 
     if (lname.length < 3) {
       return setError(
-        "js-left-form-lname-error",
-        "Last name must be at least 3 characters"
+        'js-left-form-lname-error',
+        'Last name must be at least 3 characters'
       );
     }
-    setError("js-left-form-lname-error", "");
+    setError('js-left-form-lname-error', '');
 
     leftFormDataObj.lname = lname;
 
     // e-mail
-    const email = formLeft.get("email");
+    const email = formLeft.get('email');
 
     if (!email) {
-      return setError("js-left-form-email-error", "enter email");
+      return setError('js-left-form-email-error', 'enter email');
     }
 
     const emailCheckResult = EMAIL_REGEX.test(email);
     if (!emailCheckResult) {
-      return setError("js-left-form-email-error", "correct email");
+      return setError('js-left-form-email-error', 'correct email');
     }
 
-    setError("js-left-form-email-error", "");
+    setError('js-left-form-email-error', '');
 
     leftFormDataObj.email = email;
 
     //  gender
-    const sex = formLeft.get("sex");
-    
+    const sex = formLeft.get('sex');
+
     leftFormDataObj.sex = sex;
 
     // country
-    const country = formLeft.get("country");
+    const country = formLeft.get('country');
     if (country === 'Select') {
-      return setError("js-left-form-country-error", "select country");
+      return setError('js-left-form-country-error', 'select country');
     }
 
-    setError("js-left-form-country-error", "");
+    setError('js-left-form-country-error', '');
 
     leftFormDataObj.country = country;
 
     // city
-    const city = formLeft.get("city");
+    const city = formLeft.get('city');
     const checkCityResult = CITY_REGEX.test(city);
 
-
     if (!city) {
-      return setError("js-left-form-city-error", "enter city");
+      return setError('js-left-form-city-error', 'enter city');
     }
 
     if (city.length < 2) {
       return setError(
-        ("js-left-form-city-error", "city must be at least 2 characters")
+        ('js-left-form-city-error', 'city must be at least 2 characters')
       );
     }
 
     if (!checkCityResult) {
-      return setError("js-right-form-city-error", "enter correct city name");
+      return setError('js-right-form-city-error', 'enter correct city name');
     }
 
-    setError("js-left-form-city-error", "");
+    setError('js-left-form-city-error', '');
 
     leftFormDataObj.city = city;
 
     leftFormDataArr.push(leftFormDataObj);
 
     const leftFormDataArrStr = JSON.stringify(leftFormDataArr);
-    localStorage.setItem("left-form-data", leftFormDataArrStr);
+    localStorage.setItem('left-form-data', leftFormDataArrStr);
   });
+
+  // backend
+
+  const DOMSignUpButton = document.getElementById('js-sign-up-btn');
+  const DOMFirstNameInput = document.getElementById('fname');
+  const DOMLastNameInput = document.getElementById('lname');
+  const DOMEmailInput = document.getElementById('email');
+  const DOMCityInput = document.getElementById('city');
+  const DOMSelect = document.getElementById('country');
+// Radio
+  const DOMRadios = document.getElementsByName('sex');
+  console.log(DOMRadios);
+
+  let selectedValue = null;
+
+  DOMRadios.forEach((input) => {
+    input.addEventListener('change', function () {
+      if (this.checked) {
+        selectedValue = this.value;
+        console.log('Selected value:', selectedValue);
+        return selectedValue;
+      }
+    });
+  });
+
+  if (DOMSignUpButton) {
+    DOMSignUpButton.addEventListener('click', () => {
+      const data = {
+        firstName: DOMFirstNameInput.value,
+        lastName: DOMLastNameInput.value,
+        email: DOMEmailInput.value,
+        city: DOMCityInput.value,
+        gender: selectedValue,
+        country: DOMSelect.value,
+      };
+      const dataToString = JSON.stringify(data);
+      fetch('http://localhost:3000/registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: dataToString,
+      })
+        .then((r) => r.json())
+        .then((response) => {
+          console.log(response);
+        });
+    });
+  }
 };
 
 export default registrationFormLeft;
